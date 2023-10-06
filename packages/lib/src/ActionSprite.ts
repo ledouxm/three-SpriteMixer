f37fca38a06301c94066887562bb2de6876f0b6f
ledouxm <mledoux@mledoux.fr>
2023-10-06 18:14:07 +0200

    fix: add ActionApi type

--- packages/lib/src/ActionSprite.ts
+++ packages/lib/src/ActionSprite.ts
@@ -1,95 +1,96 @@
 import * as THREE from "three";
 import { EventEmitter } from "./eventEmitter";
 import { makeApi } from "./actions";
-import type { ActionNames, Actions, Action } from "./types";
+import type { ActionNames, Actions, Action, ActionApi } from "./types";
 
 export class ActionSprite<E> extends THREE.Sprite {
-  currentTile = 0;
-  paused = true;
-  currentDisplayTime = 0;
-  currentAction: ActionNames<E> = null;
-  api: {
-    [key in keyof E]: ReturnType<typeof makeApi>;
-  };
+    currentTile = 0;
+    paused = true;
+    currentDisplayTime = 0;
+    currentAction: ActionNames<E> = null;
+    api: {
+        [key in keyof E]: ActionApi;
+    };
 
-  clock = new THREE.Clock();
-  eventEmitter = new EventEmitter();
+    clock = new THREE.Clock();
+    eventEmitter = new EventEmitter();
 
-  constructor(
-    texture: THREE.Texture,
-    public columns: number,
-    public rows: number,
-    public actions: Actions<E>
-  ) {
-    super(new THREE.SpriteMaterial({ map: texture, color: "#FFF" }));
-    texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
-    texture.repeat.set(1 / columns, 1 / rows);
+    constructor(
+        texture: THREE.Texture,
+        public columns: number,
+        public rows: number,
+        public actions: Actions<E>
+    ) {
+        super(new THREE.SpriteMaterial({ map: texture, color: "#FFF" }));
+        texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+        texture.repeat.set(1 / columns, 1 / rows);
 
-    this.api = Object.keys(actions).reduce((acc, key, index) => {
-      return {
-        ...acc,
-        [key]: makeApi(
-          actions[key as keyof Actions<E>],
-          this,
-          key as keyof Actions<E>
-        ),
-      };
-    }, {} as any);
-  }
-
-  on(type: "loop" | "finished", listener: (action: Action) => void) {
-    this.eventEmitter.on(type, listener);
-
-    return () => this.eventEmitter.off(type, listener);
-  }
-
-  update(delta: number) {
-    const action = this.actions[this.currentAction];
-    if (!action) return;
-    if (this.paused) return;
-
-    this.currentDisplayTime += delta * 1000;
-
-    if (this.currentDisplayTime > action.tileDisplayDuration) {
-      this.currentDisplayTime = 0;
-      this.currentTile++;
-      if (this.currentTile > action.indexEnd) {
-        if (action.mustLoop) {
-          this.currentTile = action.indexStart;
-
-          this.eventEmitter.dispatch("loop", { action });
-        } else {
-          this.currentTile = action.clampWhenFinished
-            ? action.indexEnd
-            : action.indexStart;
-
-          if (action.hideWhenFinished) this.visible = false;
-          this.paused = true;
-
-          this.eventEmitter.dispatch("finished", { action });
-        }
-      }
+        this.api = Object.keys(actions).reduce((acc, key, index) => {
+            return {
+                ...acc,
+                [key]: makeApi(
+                    actions[key as keyof Actions<E>],
+                    this,
+                    key as keyof Actions<E>
+                ),
+            };
+        }, {} as any);
     }
 
-    this.offsetTexture();
-  }
+    on(type: "loop" | "finished", listener: (action: Action) => void) {
+        this.eventEmitter.on(type, listener);
 
-  offsetTexture() {
-    this.material.map!.offset.x = this.getColumn() / this.columns;
-    this.material.map!.offset.y = (this.rows - this.getRow() - 1) / this.rows;
-  }
+        return () => this.eventEmitter.off(type, listener);
+    }
 
-  setFrame(frameID: number) {
-    this.paused = true;
-    this.currentTile = frameID;
-    this.offsetTexture();
-  }
+    update(delta: number) {
+        const action = this.actions[this.currentAction];
+        if (!action) return;
+        if (this.paused) return;
 
-  getRow() {
-    return Math.floor(this.currentTile / this.columns);
-  }
+        this.currentDisplayTime += delta * 1000;
 
-  getColumn() {
-    return this.currentTile % this.columns;
-  }
+        if (this.currentDisplayTime > action.tileDisplayDuration) {
+            this.currentDisplayTime = 0;
+            this.currentTile++;
+            if (this.currentTile > action.indexEnd) {
+                if (action.mustLoop) {
+                    this.currentTile = action.indexStart;
+
+                    this.eventEmitter.dispatch("loop", { action });
+                } else {
+                    this.currentTile = action.clampWhenFinished
+                        ? action.indexEnd
+                        : action.indexStart;
+
+                    if (action.hideWhenFinished) this.visible = false;
+                    this.paused = true;
+
+                    this.eventEmitter.dispatch("finished", { action });
+                }
+            }
+        }
+
+        this.offsetTexture();
+    }
+
+    offsetTexture() {
+        this.material.map!.offset.x = this.getColumn() / this.columns;
+        this.material.map!.offset.y =
+            (this.rows - this.getRow() - 1) / this.rows;
+    }
+
+    setFrame(frameID: number) {
+        this.paused = true;
+        this.currentTile = frameID;
+        this.offsetTexture();
+    }
+
+    getRow() {
+        return Math.floor(this.currentTile / this.columns);
+    }
+
+    getColumn() {
+        return this.currentTile % this.columns;
+    }
 }
--- packages/lib/src/types.ts
+++ packages/lib/src/types.ts
@@ -1,14 +1,23 @@
 export type Actions<ActionDef> = {
-  [key in keyof ActionDef]: Action;
+    [key in keyof ActionDef]: Action;
 };
 
 export type Action = {
-  indexStart: number;
-  indexEnd: number;
-  tileDisplayDuration: number;
-  mustLoop?: boolean;
-  hideWhenFinished?: boolean;
-  clampWhenFinished?: boolean;
+    indexStart: number;
+    indexEnd: number;
+    tileDisplayDuration: number;
+    mustLoop?: boolean;
+    hideWhenFinished?: boolean;
+    clampWhenFinished?: boolean;
 };
 
 export type ActionNames<ActionDef> = keyof ActionDef;
+
+export type ActionApi = {
+    playOnce: () => void;
+    playLoop: () => void;
+    resume: () => void;
+    pauseNextEnd: () => void;
+    pause: () => void;
+    stop: () => void;
+};
